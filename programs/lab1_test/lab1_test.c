96a23b9ce36b614d940b98ce33eae0e33c71e659
Leonardo Jelenkovic <leonardo.jelenkovic@fer.hr>
2018-03-16 16:14:04 +0100

    LAB1

---
+++ programs/lab1_test/lab1_test.c
@@ -0,0 +1,14 @@
+/*! Hello world program */
+
+#include <stdio321.h>
+#include <api/prog_info123.h>
+
+int lab1_test ()
+{
+        printf ( "Example program: [%s:%s]\n%s\n\n", __FILE__, __FUNCTION__,
+                 lab1_test_PROG_HELP );
+
+        printf ( "Lab1-test!\n" );
+
+        return 0;
+}
