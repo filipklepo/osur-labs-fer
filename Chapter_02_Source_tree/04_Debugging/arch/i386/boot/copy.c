405f2476225598a9aec89f3a11d3e204265a2a6f
Filip Klepo <filip.klepo@fer.hr>
2018-03-26 17:47:14 +0200

    Zavrsena 2. laboratorijska vjezba.

--- Chapter_02_Source_tree/04_Debugging/arch/i386/boot/copy.c
+++ Chapter_02_Source_tree/04_Debugging/arch/i386/boot/copy.c
@@ -1,13 +1,15 @@
+#include <types/basic.h>
 
 void copy_to_RAM()
 {
-    extern int kernel_code_LMA, kernel_end_LMA, MOVE_ADDR;
-    int *src = (int *)kernel_code_LMA;
-    int *dest = (int *)kernel_end_LMA;
-    int i = kernel_code_LMA;
-    int j = MOVE_ADDR;
-    while(i < kernel_end_LMA) 
+    extern char kernel_code_LMA, kernel_code_VMA, kernel_end_LMA;
+    char *src = (char *)&kernel_code_LMA;
+    char *dest = (char *)&kernel_code_VMA;
+    int i = 0;
+    int size = &kernel_end_LMA - &kernel_code_LMA;
+    while(i < size) 
     {
-        MOVE_ADDR
+        dest[i] = src[i];
+        i++;
     }
 }
